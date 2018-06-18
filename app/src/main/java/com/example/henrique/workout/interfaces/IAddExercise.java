44c657f2ead31a39c786e527c1b112da838df734
Henrique Ramos <rickeight@hotmail.com>
2018-06-17 23:02:00 -0300

    Final Countdown

--- app/src/main/java/com/example/henrique/workout/interfaces/IAddExercise.java
+++
@@ -1,7 +0,0 @@
-package com.example.henrique.workout.interfaces;
-
-import com.example.henrique.workout.models.Exercise;
-
-public interface IAddExercise {
-    void onExerciseAdd(Exercise exercise,boolean editMode, int position);
-}
